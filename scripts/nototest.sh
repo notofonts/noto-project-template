3c1a1452d2bfc72b97bbc1b00bcc532916264c16
Simon Cozens <simon@simon-cozens.org>
2022-04-05 17:05:52 +0100

    Split out testing to script as we will test multiple targets

---
+++ scripts/nototest.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+mkdir -p out/ out/fontbakery
+
+# Hinted and unhinted fonts must pass notofonts profile
+NOTO_TARGET_OUTPUTS="$(find fonts/unhinted/ttf -type f) $(find fonts/hinted/ttf -type f)"
+
+if [ $NOTO_TARGET_OUTPUTS = " " ]
+then
+	echo "No fonts were found!"
+	exit 1
+fi
+
+. venv/bin/activate
+fontbakery check-notofonts -l WARN --succinct --badges out/badges --html out/fontbakery/notofonts-report.html --ghmarkdown out/fontbakery/notofonts-report.md $NOTO_TARGET_OUTPUTS
+NOTO_EXIT=$?
+
+# "Full" fonts (if any) must pass googlefonts profile
+GOOGLE_TARGET_OUTPUTS="$(find fonts/full/ttf -type f)"
+if [ -n $GOOGLE_TARGET_OUTPUTS ]
+then
+	fontbakery check-googlefonts -l WARN --succinct --badges out/badges --html out/fontbakery/googlefonts-report.html --ghmarkdown out/fontbakery/googlefonts-report.md $GOOGLE_TARGET_OUTPUTS
+	GF_EXIT=$?
+fi
+
+if [ $GF_EXIT ] || [ $NOTO_EXIT ]
+then
+	exit 1
+fi
+
+exit 0
